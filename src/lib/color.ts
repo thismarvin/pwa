e6fc8dafdb51cc8f1908cd35353df49084af926d
marvin <marvin@thismarvin.com>
2022-01-20 15:33:42 -0600

    chore(lib): imported pickles library

---
+++ src/lib/color.ts
@@ -0,0 +1,81 @@
+import { clamp } from "./mathExt";
+
+function _clampValue(value: number): number {
+	return clamp(value, 0, 255);
+}
+
+export default class Color {
+	public readonly r: number;
+	public readonly g: number;
+	public readonly b: number;
+	public readonly a: number;
+
+	constructor(r: number, g: number, b: number, a: number) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.a = a;
+	}
+
+	public static default(): Color {
+		return new Color(0, 0, 0, 255);
+	}
+
+	public static multiply(color: Color, alpha: number): Color {
+		const tmp = clamp(alpha, 0, 1);
+
+		const r = _clampValue(color.r * tmp);
+		const g = _clampValue(color.g * tmp);
+		const b = _clampValue(color.b * tmp);
+		const a = 255 * tmp;
+
+		return new Color(r, g, b, a);
+	}
+
+	public static fromHex(value: number, a?: number): Color {
+		const r = _clampValue(value >> 16);
+		const g = _clampValue((value & 0x00ff00) >> 8);
+		const b = _clampValue(value & 0x0000ff);
+
+		if (a === undefined) {
+			return new Color(r, g, b, 255);
+		}
+
+		const temp = new Color(r, g, b, 255);
+
+		return Color.multiply(temp, a);
+	}
+
+	public static fromHexString(value: string, a?: number): Color {
+		const sanitized = (() => {
+			let sanitized = value.toLowerCase().trim();
+
+			if (sanitized.substring(0, 1) === "#") {
+				sanitized = sanitized.substring(1);
+			}
+
+			return sanitized;
+		})();
+
+		if (!/^[\da-f]{6}$/.test(sanitized))
+			throw new TypeError("The given string could not be parsed as a hexadecimal value.");
+
+		const hexAsNumber = parseInt(sanitized, 16);
+
+		return Color.fromHex(hexAsNumber, a);
+	}
+
+	public static fromRgb(r: number, g: number, b: number, a?: number): Color {
+		const sanitizedR = _clampValue(r);
+		const sanitizedG = _clampValue(g);
+		const sanitizedB = _clampValue(b);
+
+		if (a === undefined) {
+			return new Color(sanitizedR, sanitizedG, sanitizedB, 255);
+		}
+
+		const temp = new Color(sanitizedR, sanitizedG, sanitizedB, 255);
+
+		return Color.multiply(temp, a);
+	}
+}
---
+++ src/lib/pickles.ts
@@ -0,0 +1,214 @@
+import * as WebGL from "./webgl";
+import Matrix4 from "$lib/matrix4";
+import Vector3 from "$lib/vector3";
+import type Color from "./color";
+
+function _createOrthographicCamera(
+	position: Vector3,
+	forward: Vector3,
+	up: Vector3,
+	width: number,
+	height: number,
+	near: number,
+	far: number
+): Matrix4 {
+	const view = Matrix4.createLookAt(position, Vector3.add(position, forward), up);
+	const projection = Matrix4.createOrthographic(width, height, near, far);
+
+	return Matrix4.multiply(view, projection);
+}
+
+const source = {
+	vertex: `
+	uniform mat4 camera;
+
+	attribute vec3 a_position;
+	attribute vec3 a_translation;
+	attribute vec4 a_color;
+
+	varying lowp vec4 v_color;
+
+	void main() {
+		gl_Position = camera * vec4(a_position + a_translation, 1);
+		v_color = a_color;
+	}
+`,
+	fragment: `
+	varying lowp vec4 v_color;
+
+	void main() {
+		gl_FragColor = v_color;
+	}
+`,
+};
+
+export default class Pixels {
+	public readonly canvas: HTMLCanvasElement;
+	public readonly width: number;
+	public readonly height: number;
+
+	public get area(): number {
+		return this.width * this.height;
+	}
+
+	private readonly gl: WebGL2RenderingContext;
+	private readonly program: WebGLProgram;
+	private readonly camera: Matrix4;
+	private readonly vertexPositionsBuffer: WebGLBuffer;
+	private readonly vertexTranslationsBuffer: WebGLBuffer;
+	private readonly indexBuffer: WebGLBuffer;
+
+	private vertexColors: Uint8Array;
+	private vertexColorsBuffer: WebGLBuffer;
+	private dirty: boolean;
+
+	constructor(canvas: HTMLCanvasElement, width: number, height: number) {
+		this.canvas = canvas;
+		this.width = width;
+		this.height = height;
+
+		this.gl = canvas.getContext("webgl2");
+
+		if (this.gl === null) {
+			throw new TypeError("Could not get 'webgl2' context.");
+		}
+
+		this.program = WebGL.createProgram(this.gl, source.vertex, source.fragment);
+
+		this.camera = _createOrthographicCamera(
+			new Vector3(this.width * 0.5, -this.height * 0.5, 1),
+			Vector3.FORWARD,
+			Vector3.UP,
+			this.width,
+			this.height,
+			0.125,
+			8
+		);
+
+		const vertexPositions = new Int8Array([0, 0, 0, 0, -1, 0, 1, -1, 0, 1, 0, 0]);
+		const vertexTranslations = new Int16Array(this.area * 3).fill(0);
+		const indices = new Uint8Array([0, 1, 2, 0, 2, 3]);
+
+		for (let y = 0; y < this.height; ++y) {
+			for (let x = 0; x < this.width; ++x) {
+				const index = y * this.width + x;
+				const j = index * 3;
+
+				vertexTranslations[j + 0] = x;
+				vertexTranslations[j + 1] = -y;
+				vertexTranslations[j + 2] = 0;
+			}
+		}
+
+		this.vertexColors = new Uint8Array(this.area * 4).fill(0);
+
+		for (let y = 0; y < this.height; ++y) {
+			for (let x = 0; x < this.width; ++x) {
+				const index = y * this.width + x;
+				const j = index * 4;
+
+				this.vertexColors[j + 0] = 0;
+				this.vertexColors[j + 1] = 0;
+				this.vertexColors[j + 2] = 0;
+				this.vertexColors[j + 3] = 255;
+			}
+		}
+
+		this.dirty = false;
+
+		this.gl.useProgram(this.program);
+		{
+			const location = this.gl.getUniformLocation(this.program, "camera");
+			this.gl.uniformMatrix4fv(location, false, this.camera.data);
+		}
+
+		this.vertexPositionsBuffer = WebGL.createBufferInit(
+			this.gl,
+			WebGL.BufferUsage.Vertex,
+			vertexPositions
+		);
+		{
+			const index = this.gl.getAttribLocation(this.program, "a_position");
+
+			this.gl.bindBuffer(WebGL.BufferUsage.Vertex, this.vertexPositionsBuffer);
+			this.gl.enableVertexAttribArray(index);
+			this.gl.vertexAttribPointer(index, 3, WebGL.AttributeType.Byte, false, 3, 0);
+			this.gl.vertexAttribDivisor(index, 0);
+		}
+
+		this.vertexTranslationsBuffer = WebGL.createBufferInit(
+			this.gl,
+			WebGL.BufferUsage.Vertex,
+			vertexTranslations
+		);
+		{
+			const index = this.gl.getAttribLocation(this.program, "a_translation");
+
+			this.gl.bindBuffer(WebGL.BufferUsage.Vertex, this.vertexTranslationsBuffer);
+			this.gl.enableVertexAttribArray(index);
+			this.gl.vertexAttribPointer(index, 3, WebGL.AttributeType.Short, false, 6, 0);
+			this.gl.vertexAttribDivisor(index, 1);
+		}
+
+		this.vertexColorsBuffer = WebGL.createBufferInit(
+			this.gl,
+			WebGL.BufferUsage.Vertex,
+			this.vertexColors
+		);
+		{
+			const index = this.gl.getAttribLocation(this.program, "a_color");
+
+			this.gl.bindBuffer(WebGL.BufferUsage.Vertex, this.vertexColorsBuffer);
+			this.gl.enableVertexAttribArray(index);
+			this.gl.vertexAttribPointer(index, 4, WebGL.AttributeType.UnsignedByte, true, 4, 0);
+			this.gl.vertexAttribDivisor(index, 1);
+		}
+
+		this.indexBuffer = WebGL.createBufferInit(this.gl, WebGL.BufferUsage.Index, indices);
+		{
+			this.gl.bindBuffer(WebGL.BufferUsage.Index, this.indexBuffer);
+		}
+	}
+
+	public clear(color: Color): void {
+		for (let y = 0; y < this.height; ++y) {
+			for (let x = 0; x < this.width; ++x) {
+				const index = y * this.width + x;
+				const j = index * 4;
+
+				this.vertexColors[j + 0] = color.r;
+				this.vertexColors[j + 1] = color.g;
+				this.vertexColors[j + 2] = color.b;
+				this.vertexColors[j + 3] = color.a;
+			}
+		}
+
+		this.dirty = true;
+	}
+
+	public set(x: number, y: number, color: Color): void {
+		if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+			throw new Error("Index out of bounds.");
+		}
+
+		const index = y * this.width + x;
+		const j = index * 4;
+
+		this.vertexColors[j + 0] = color.r;
+		this.vertexColors[j + 1] = color.g;
+		this.vertexColors[j + 2] = color.b;
+		this.vertexColors[j + 3] = color.a;
+
+		this.dirty = true;
+	}
+
+	public draw(): void {
+		if (this.dirty) {
+			WebGL.setVertexBufferData(this.gl, this.vertexColorsBuffer, this.vertexColors);
+
+			this.dirty = false;
+		}
+
+		this.gl.drawElementsInstanced(WebGL.DrawMode.Triangles, 6, this.gl.UNSIGNED_BYTE, 0, this.area);
+	}
+}
