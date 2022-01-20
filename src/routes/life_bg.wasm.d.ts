2a6f17da79b5845dbad3c437cf5a97dce490179f
marvin <marvin@thismarvin.com>
2022-01-20 15:38:39 -0600

    fix: removed unused .d.ts file
    `npm run bulid` does not work if the following file is included...

--- src/routes/life_bg.wasm.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function __wbg_world_free(a: number): void;
-export function world_new(a: number, b: number): number;
-export function world_contents(a: number): number;
-export function world_simulate(a: number): void;
-export function world_get(a: number, b: number, c: number): number;
-export function world_set(a: number, b: number, c: number, d: number): void;
